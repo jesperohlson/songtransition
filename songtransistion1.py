c0b3e9e7af160d9b0a431ec87b5950b332f1ce62
matthewdomanchuk <matthewwdomanchuk@gmail.com>
2025-05-14 21:59:22 -0500

    First

---
+++ songtransistion1.py
@@ -0,0 +1,14 @@
+from pydub import AudioSegment
+
+# Load songs
+song1 = AudioSegment.from_file("song1.mp3")
+song2 = AudioSegment.from_file("song2.mp3")
+
+# Duration of crossfade (in milliseconds)
+crossfade_duration = 5000
+
+# Combine with crossfade
+mixed = song1.append(song2, crossfade=crossfade_duration)
+
+# Export result
+mixed.export("seamless_mix.mp3", format="mp3")
