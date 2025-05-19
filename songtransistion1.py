e853c0df4af7ec7b061c78589e2fda6ffc176239
jesperohlson <ohlson@purdue.edu>
2025-05-19 12:02:31 -0500

    Added Beat detection function

--- songtransistion1.py
+++ songtransistion1.py
@@ -1,9 +1,17 @@
 from pydub import AudioSegment
+import librosa
 
 # Audio Normalization Function
 def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
+
+#Beat Detection Function
+def get_beats(file):
+    y, sr = librosa.load(file)
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = librosa.frames_to_time(beats, sr=sr)
+    return tempo, beat_times
 
 # Load songs
 song1 = AudioSegment.from_file("lilbaby-grace.mp3")
@@ -13,6 +21,9 @@ song2 = AudioSegment.from_file("playboicarti-location.mp3")
 target_dBFS = -20.0  # Good starting point
 song1 = match_target_amplitude(song1, target_dBFS)
 song2 = match_target_amplitude(song2, target_dBFS)
+
+#song1.export("song1_audio.mp3", format="mp3")
+#song2.export("song2_audio.mp3", format="mp3")
 
 # Duration of crossfade (in milliseconds)
 crossfade_duration = 5000
