1b150640317c6badb0f1ba36a2c27e4e65bccba3
jesperohlson <ohlson@purdue.edu>
2025-05-19 12:11:17 -0500

    added song silencing

--- songtransistion1.py
+++ songtransistion1.py
@@ -1,4 +1,5 @@
 from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
 import librosa
 
 # Audio Normalization Function
@@ -6,12 +7,21 @@ def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
-#Beat Detection Function
+# Beat Detection Function
 def get_beats(file):
     y, sr = librosa.load(file)
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
     beat_times = librosa.frames_to_time(beats, sr=sr)
     return tempo, beat_times
+
+# Trimming silence function
+def trim_silence(sound, silence_thresh=-40, min_silence_len=1000):
+    nonsilent = detect_nonsilent(sound, min_silence_len, silence_thresh)
+    if nonsilent:
+        start_trim = nonsilent[0][0]
+        end_trim = nonsilent[-1][1]
+        return sound[start_trim:end_trim]
+    return sound
 
 # Load songs
 song1 = AudioSegment.from_file("lilbaby-grace.mp3")
@@ -22,8 +32,12 @@ target_dBFS = -20.0  # Good starting point
 song1 = match_target_amplitude(song1, target_dBFS)
 song2 = match_target_amplitude(song2, target_dBFS)
 
-#song1.export("song1_audio.mp3", format="mp3")
-#song2.export("song2_audio.mp3", format="mp3")
+# Trimming the silence
+song1 = trim_silence(song1)
+song2 = trim_silence(song2)
+
+song1.export("song1_audio.mp3", format="mp3")
+song2.export("song2_audio.mp3", format="mp3")
 
 # Duration of crossfade (in milliseconds)
 crossfade_duration = 5000
